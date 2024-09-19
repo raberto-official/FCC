a72cf423ce32ec4844316e2ad6ab5a02f22d3ab8
XxCosmicCloudxX <raberto.official@gmail.com>
2024-09-18 19:06:46 -0700

    Progress made, working on getting long decimals out of the problem

--- JavaScript_Datastructures/Cash_Register/script.js
+++ JavaScript_Datastructures/Cash_Register/script.js
@@ -1,4 +1,4 @@
-let price = 19.5;
+let price = 11.95;
 let cid = [
     ['PENNY', 1.01],
     ['NICKEL', 2.05],
@@ -11,50 +11,27 @@ let cid = [
     ['ONE HUNDRED', 100]
     ];
 
-// document.addEventListener('DOMContentLoaded', (event) => {
+let cidTotal = cid.reduce((accumulator, currentValue) => accumulator + currentValue[1], 0).toFixed(2);
 
-     
-    let button = document.getElementById('purchase-btn');
-    let changeDue = document.getElementById('change-due');
-    let cidText = document.getElementById('cidText');
+let button = document.getElementById('purchase-btn');
+let changeDue = document.getElementById('change-due');
+let cidText = document.getElementById('cidText');
+
+button.addEventListener('click', () => {
     
-
-
     const insufficient = 'Status: INSUFFICIENT_FUNDS';
     const closed = 'Status: CLOSED';
     const open = 'Status: OPEN';
 
-
-
-    function getChange(cash, price) {
-        let change = (cash - price) * 100 /100;
-        // tried out this method to get total and it's super cool had trouble accessing cid[i][1] till this solution came up
-        let cidTotal = cid.reduce((accumulator, currentValue) => accumulator + currentValue[1], 0).toFixed(2);
-            
-
-        if (cash < price) {
-            alert('Customer does not have enough money to purchase the item');
-        }else if(cash == price) {
-            changeDue.innerHTML = "No change due - customer paid with exact cash";
-        }else if(change > cidTotal){
-            changeDue.innerHTML = insufficient;
-        }else {
-            //this is to clear previous input 
-            // changeDue.innerHTML = '';
-            changeDue.innerHTML = open + ' ' + changeCalc(change).map(i => `${i[0]}: ${i[1]}`).join(', ');
-            cidText.innerHTML = cid;
-            
-        }
-    }
+    let cash = parseFloat(document.getElementById('cash').value);
+    let change = (cash - price) * 100 /100;
 
     function changeCalc(change) {  
-        // does this need to be here since it's up there already on line 31
-        let cidTotal = cid.reduce((accumulator, currentValue) => accumulator + currentValue[1], 0).toFixed(2);
-        // let changeCopy = change;
+        // does this need to be here since it's up there already in getChange()
         let changeBack = [];
         //using this copy as to not alter the original, though in a real world situation might be better to have the drawer set as an input or maybe a separate const depending on policy
         let cidCopy = [...cid].reverse();
-        
+            
         //values of each coin, initially I tried with just the values instead of the names too but i've gone through so many iterations, trying something different...
         let denominations = [
             ['ONE HUNDRED', 100.00],
@@ -78,39 +55,42 @@ let cid = [
                 let coinValue = Math.floor(change / value) * value;
                 //this one picks the lowest amount so you don't run over your limit of coins? kind of how it was explained to me
                 cidCopy[i][1] -= coinValue;
-                cidTotal -= coinValue
+                // cidTotal -= coinValue;
                 let availableInCid = Math.min(coinValue, cidValue);
-                console.log(changeDue);
-                console.log(cidTotal)
-                
+                    
                 if (availableInCid > 0) {
                     changeBack.push([name, availableInCid]);
                     change -= availableInCid;
-                    change = Math.round(change * 100) / 100;
-                 
+                    change = Math.round(change * 100) / 100;                 
                 };
 
-                // for (let i = 0; i < cid.length; i++) {
-                //     cid.pop(cid[i]);
-                //     cid.push(cidCopy[i]);
-                // }
             };
-            // console.log(changeDue.value)
+                
         };
         return changeBack;
-        
+            
     };
-
-    console.log(changeDue.value)
-
-    cidText.append()
-
-    button.addEventListener('click', () => {
-        let cash = document.getElementById('cash').value;
         
-        getChange(cash, price);
-    });
+    if (cash < price) {
+        alert('Customer does not have enough money to purchase the item');
+    }else if(cash == price) {
+        const exactNode = document.createTextNode("No change due - customer paid with exact cash");
+        changeDue.appendChild(exactNode);
+    }else if(change > cidTotal){
+        //need to expand on this like what happens if i run out of change on a specific coin
+        const insufficientNode = document.createTextNode("Status: INSUFFICIENT_FUNDS");
+        changeDue.appendChild(insufficientNode);
+    }else {
+        const changeNode = document.createTextNode(open + ' ' + changeCalc(change).map(i => `${i[0]}: ${i[1]}`).join(', '))
+        changeDue.appendChild(changeNode);
+        const cidNode = document.createTextNode(cid);
+        cidText.appendChild(cidNode);
+        // to test
+        console.log(changeNode);
 
-// })
+    }
 
-// Struggling still also found out if i repeatedly press the purchase button things get wonky after a while
+
+
+    console.log(cidTotal)
+});
