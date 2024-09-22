2788d25a609621e1f111155e668448130f92f97f
XxCosmicCloudxX <raberto.official@gmail.com>
2024-09-21 17:15:14 -0700

    big progress, not doen yet

--- JavaScript_Datastructures/Cash_Register/script.js
+++ JavaScript_Datastructures/Cash_Register/script.js
@@ -49,44 +49,43 @@ button.addEventListener('click', () => {
             let name = denominations[i][0];
             let value = denominations[i][1];
             let cidValue = cidCopy.find(i => i[0] === name)[1];
+            
+                if (change >= value && cidValue > 0) {
+                    //determines coin value I know it's in the name but it's like having a dollar and then breaking it up based on the value of change available 
+                    let coinValue = Math.floor(change / value) * value;
+                    //this one picks the lowest amount so you don't run over your limit of coins? kind of how it was explained to me
+                    cidCopy[i][1] -= coinValue;
+                    // cidTotal -= coinValue;
+                    let availableInCid = Math.min(coinValue, cidValue);
+                        
+                    if (availableInCid > 0) {
+                        changeBack.push([name, availableInCid]);
+                        change -= availableInCid;
+                        change = Math.round(change * 100) / 100;
+                    }  else {
+                        changeDue.textContent = "Status: INSUFFICIENT_FUNDS";
+                    }
 
-            if (change >= value && cidValue > 0) {
-                //determines coin value I know it's in the name but it's like having a dollar and then breaking it up based on the value of change available 
-                let coinValue = Math.floor(change / value) * value;
-                //this one picks the lowest amount so you don't run over your limit of coins? kind of how it was explained to me
-                cidCopy[i][1] -= coinValue;
-                // cidTotal -= coinValue;
-                let availableInCid = Math.min(coinValue, cidValue);
-                    
-                if (availableInCid > 0) {
-                    changeBack.push([name, availableInCid]);
-                    change -= availableInCid;
-                    change = Math.round(change * 100) / 100;                 
                 };
-
-            };
                 
         };
         return changeBack;
             
     };
         
-    if (cash < price) {
+    if(cash == ''){
+        alert('Please input a cash amount')
+    }else if (cash < price) {
         alert('Customer does not have enough money to purchase the item');
-    }else if(cash == price) {
-        const exactNode = document.createTextNode("No change due - customer paid with exact cash");
-        changeDue.appendChild(exactNode);
+    }else if(cash === price) {
+        changeDue.textContent = "No change due - customer paid with exact cash";
     }else if(change > cidTotal){
         //need to expand on this like what happens if i run out of change on a specific coin
         const insufficientNode = document.createTextNode("Status: INSUFFICIENT_FUNDS");
         changeDue.appendChild(insufficientNode);
     }else {
-        const changeNode = document.createTextNode(open + ' ' + changeCalc(change).map(i => `${i[0]}: ${i[1]}`).join(', '))
-        changeDue.appendChild(changeNode);
-        const cidNode = document.createTextNode(cid);
-        cidText.appendChild(cidNode);
-        // to test
-        console.log(changeNode);
+        changeDue.textContent = open + ' ' + changeCalc(change).map(i => `${i[0]}: $${i[1]}`).join(', ');
+        cidText.textContent = cid;
 
     }
 
